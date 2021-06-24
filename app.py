717e7ee31b0c71ff50ac439e005417f0aa698f8f
Anwaar <shahanwaar.khalid8@gmail.com>
2021-06-24 15:42:05 +0530

    clean up

--- app.py
+++ app.py
@@ -13,41 +13,49 @@ from model import ResNet200D
 
 
 def main():
+    #Load the trained model
     model = ResNet200D()
     model.load_state_dict(torch.load("./resnet200d.pth")['model'])
     model.eval()
     
+    #Heading
     st.write("""
              # Incorrect Placement of Chest Catheter Detector
              """
              )
     
+    #Description
     st.write("A Resnet-200D based image classification model to detect incorrect place of catheters on chest X-Rays")
     
+    #prompt to uplaod an the X-Ray Image
     file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
-    #
     if file is None:
         st.text("You haven't uploaded an image file")
     else:
+        #apply some basic transformations
         image = Image.open(file)
         st.image(image, use_column_width=True)
         image = np.array(image)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #do a forward pass through tht model
         prediction = import_and_predict(image, model)
         prediction = pd.DataFrame({'ETT-Abnormal' : prediction[0],
-                      'ETT-Borderline': prediction[1],
-                      'ETT-Normal': prediction[2],
-                      'NGT-Abnormal': prediction[3],
-                      'NGT-Borderline': prediction[4],
-                      'NGT-Incorrectly Imaged': prediction[5],
-                      'NGT-Normal': prediction[6],
-                      'CVC-Abnormal': prediction[7],
-                      'CVC-Borderline': prediction[8],
-                      'CVC-Normal': prediction[9],
-                      'Swan Ganz Catheter': prediction[10],
-                      }
+                                   'ETT-Borderline': prediction[1],
+                                   'ETT-Normal': prediction[2],
+                                   'NGT-Abnormal': prediction[3],
+                                   'NGT-Borderline': prediction[4],
+                                   'NGT-Incorrectly Imaged': prediction[5],
+                                   'NGT-Normal': prediction[6],
+                                   'CVC-Abnormal': prediction[7],
+                                   'CVC-Borderline': prediction[8],
+                                   'CVC-Normal': prediction[9],
+                                   'Swan Ganz Catheter': prediction[10],
+                                   }
                       , index = [0])
+        #print the probablities
         st.write(prediction)
+        #if any of the catheter's has > 50% probablity in 
+        #borderline or abnormal category, alert the user.
         for i in range(len(prediction.columns)):
             if(prediction.iloc[0,i] > 0.5):
                 if(prediction.columns[i].split('-')[1] in ['Borderline', 'Abnormal']):
